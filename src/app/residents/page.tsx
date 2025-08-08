3cddb900627a2b7b88add548ebc9636e6cfde23b
Jake Rider <hello@jakerider.io>
2025-08-08 14:36:28 -0400

    Create residents table

--- src/app/residents/page.tsx
+++ src/app/residents/page.tsx
@@ -6,5 +6,28 @@ const db = drizzle(process.env.DB_FILE_NAME!);
 export default async function ResidentsPage() {
   const residents = await db.select().from(residentsTable);
 
-  return <p>{JSON.stringify(residents)}</p>;
+  return (
+    <main className="container">
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Title</th>
+            <th scope="col">Hometown</th>
+          </tr>
+        </thead>
+        <tbody>
+          {residents.map((resident) => (
+            <tr key={resident.id}>
+              <th scope="row">{resident.name}</th>
+              <td>{resident.age}</td>
+              <td>{resident.title}</td>
+              <td>{resident.hometown}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
 }
