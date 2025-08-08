c9889f6f65c97bd72173c6efe8d82acdb82c31d1
Jake Rider <hello@jakerider.io>
2025-08-08 12:31:43 -0400

    Remove tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { usersTable } from '@/db/schema';
-import { drizzle } from 'drizzle-orm/libsql';
-import { eq } from 'drizzle-orm';
-
-const db = drizzle(process.env.DB_FILE_NAME!);
-
 export default async function Home() {
   return <p>Hello, world!</p>;
 }
